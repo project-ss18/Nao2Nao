10d658e4b34c45e4996dbd9111094721c9a3d057
enesm <enes.oezel@hs-furtwangen.de>
2018-05-15 15:00:25 +0200

    View - Menu und Roboterliste hinzugefügt

--- src/model/robot/Robot.java
+++ src/model/robot/Robot.java
@@ -1,30 +1,39 @@
 package model.robot;
 
-import com.aldebaran.qi.helper.ALProxy;
-import com.aldebaran.qi.helper.proxies.ALRobotPosture;
 import controller.Connection;
 
 public class Robot {
+    private static int id_Counter = 0;
+    private String[] args= new String[1];
 
+    private String name;
     private final String IP_ADDRESS;
     private final int ID;
-    private final Connection CONNECTION;
+
+    private final Connection CONNECTION=null;
     private final String PORT = "9559";
 
 
-    public Robot(String address, int id, String[] args) throws Exception {
+    public Robot(String address, String name) throws Exception {
+        id_Counter++;
+        this.name=name;
         IP_ADDRESS = address;
-        ID = id;
-        CONNECTION = new Connection(this.IP_ADDRESS, args);
+        ID = id_Counter;
+        //CONNECTION = new Connection(this.IP_ADDRESS, args);
 
     }
-    //
+
     public String getIPAdress() {
         return IP_ADDRESS;
     }
 
-    public int get_ID(){
-        return ID;
+    public String []toStringArray(){
+        String[] temp = new String[2];
+        temp = new String[]{Integer.toString(this.ID), this.name, this.getIP_ADDRESS()};
+        return temp;
+    }
+    public String get_ID(){
+        return Integer.toString(ID);
     }
 
     public void ping() throws Exception{
@@ -38,4 +47,13 @@ public class Robot {
     public void gesture(String args)throws Exception{
         CONNECTION.gesture(args);
     }
+    public String getName() {
+        return name;
+    }
+
+    public String getIP_ADDRESS() {
+        return IP_ADDRESS;
+    }
+
+    //public void edit(String nam)
 }
