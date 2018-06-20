cb908b42f59cb4315706ce6752eaeabeab2537b2
enesm <enes.oezel@hs-furtwangen.de>
2018-06-20 17:27:25 +0200

    gitignore anpassen

--- src/GuiMain.java
+++ src/GuiMain.java
@@ -1,8 +1,8 @@
-package view;
+import view.Menu;
 
 import javax.swing.*;
 
-public class Testmain {
+public class GuiMain {
     public static void main(String[] args) throws ClassNotFoundException, UnsupportedLookAndFeelException, InstantiationException, IllegalAccessException {
         UIManager.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
         Menu m = new Menu();
